5225ba5e571c0d7e34658327b406deb7f9e33a1a
kodakar <106800462+kodakar@users.noreply.github.com>
2023-04-05 19:48:11 +0900

    Add files via upload

---
+++ IWAIGAMES/Server_game.py
@@ -0,0 +1,26 @@
+import socket
+import threading
+
+class Server():#サーバー側(受信側の処理)
+    def __init__(self,q, host, port):
+        self.q = q
+        self.host = host
+        self.port = port
+        
+        self.bufsize = 1024
+
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sock.bind((self.host, self.port))
+        self.thread = threading.Thread(target = self.c2s, daemon=True)
+        self.thread.start()
+
+    def c2s(self):#サーバー側へ受信
+        try:
+            while True:
+                msg, cli_addr = self.sock.recvfrom(self.bufsize)
+                msg.decode('utf-8')
+                self.q.put(msg)
+
+        except Exception as e:
+            print(e)
+    
